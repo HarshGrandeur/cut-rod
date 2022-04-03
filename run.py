82f5cc3b9ae1db700b99780bf834a9f5bba6c490
HarshGrandeur <hrshgpt73@gmail.com>
2022-04-03 17:54:56 -0400

    read the file

---
+++ run.py
@@ -0,0 +1,13 @@
+import sys, getopt
+from scheduler import Scheduler
+
+def main(argv):
+    file_name = argv[1]
+    n_mappers = argv[2]
+    n_reducers =  argv[3]
+    sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
+    sc.split_input_files()
+
+
+if __name__ == "__main__":
+   main(sys.argv)
--- scheduler.py
+++ scheduler.py
@@ -1,35 +1,43 @@
 import os
+import math
+import sys
 import json
 import settings
 from multiprocessing import Process
-
-class FileHandler:
-
-    def __init__(self, input_file_path, output_dir):
-        """
-        Note: the input file path should be given for splitting.
-        The output directory is needed for joining the outputs.
-
-        :param input_file_path: input file path
-        :param output_dir: output directory path
-
-        """
-        self.input_file_path = input_file_path
-        self.output_dir = output_dir
 
 class Scheduler:
     """"
     Initialize scheduler with default paramerters
     """
     def __init__(self, n_mappers=settings.default_n_mappers, n_reducers=settings.default_n_reducers,
-    input_dir="input_files",output_dir="output_files"):
+    input_dir="input_files",output_dir="output_files", file_name=None):
         self.n_mappers = n_mappers
         self.n_reducers = n_reducers
         self.input_dir = input_dir
         self.output_dir = output_dir
+        self.file_name = file_name
 
     """
     Split input file into no of mappers
     """
     def split_input_files(self):
+        try:
+            self.file_path = self.input_dir + "/" + self.file_name
+            self.file_size = os.stat(self.file_path)
+            # self.n_file_split = math.floor(self.file_size / self.n_mappers) 
+            print("file_size: ", self.file_size.st_size)
+
+        except FileNotFoundError:
+            print(f"File {self.file_name} does not exist.  Aborting")
+            sys.exit(1)
+
+        ## read the file contents
+        print("path" , self.file_path)
+        ## print the file contents
+        with open(self.file_path, 'r') as f:
+            contents = f.readlines()
+            for line in contents:
+                print(line)
+
+        
         
