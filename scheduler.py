e9cb63676178a8ad3e84f428f3d2bc201fe44a8a
HarshGrandeur <hrshgpt73@gmail.com>
2022-04-17 13:14:24 -0400

    Fixed all issues, used manager to manager queues

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ from map_reduce import *
 from math import ceil
 from multiprocessing import Process, Queue
 from utils import chunked
-import signal
+from multiprocessing import Manager
 
 class Scheduler:
     """"
@@ -16,8 +16,10 @@ class Scheduler:
         self.input_dir = input_dir
         self.output_dir = output_dir
         self.file_name = file_name
-        self.queue = Queue()
+        self.manager = Manager()
+        self.queue = self.manager.Queue()
         self.combined = {}
+
 
     """
     Split input file into no of mappers
@@ -59,49 +61,53 @@ class Scheduler:
             file_descriptors[mapper].close()
 
     def launch_mappers(self):
-        processes = []
-        # calculate running time
-        start_time = time.time()
-        
-        # Start all the mappers in parallel
-        for i in range(self.n_mappers):
-            file_path = self.input_dir + "/" + str(i) + ".txt"
-            p = Process(target=self.mapper, args=(file_path, map, self.queue))
+        ## Using manager, as it manager queue in a separate process and it's not flused 
+        ## after the child process exits
+        with Manager() as manager:
+            processes = []
+            queue = manager.Queue()
+            # calculate running time
+            start_time = time.time()
+            
+            # Start all the mappers in parallel
+            for i in range(self.n_mappers):
+                file_path = self.input_dir + "/" + str(i) + ".txt"
+                p = Process(target=self.mapper, args=(file_path, map, queue))
+                p.start()
+                processes.append(p)
+
+            # Next we perform sorting (this is the eager setting)
+            # For the lazy setting, we need to join all the mappers
+            # in the list `processes` (this means that sorting
+            # will have to wait until all mappers are done)
+            for p in processes:
+                p.join()
+            processes = [] 
+            output = manager.Queue()
+            p = Process(target=self.sort, args = [queue, output])
             p.start()
             processes.append(p)
 
-        # Next we perform sorting (this is the eager setting)
-        # For the lazy setting, we need to join all the mappers
-        # in the list `processes` (this means that sorting
-        # will have to wait until all mappers are done)
-        for p in processes:
-            p.join()
-        processes = []  
-        output = Queue()
-        p = Process(target=self.sort, args = [self.queue, output])
-        p.start()
-        processes.append(p)
+            for p in processes:
+                p.join()
+            processes = []
 
-        for p in processes:
-            p.join()
-        processes = []
+            self.combined = output.get()
+            
+            # Now here we start the reducers
+            i = 0
+            for chunk in chunked(self.combined.items(), ceil(len(self.combined) / self.n_reducers)):
+                file_path = self.output_dir + "/" + str(i) + ".txt"
+                i += 1
+                p = Process(target=self.reducer, args=(file_path, chunk))
+                p.start()
+                processes.append(p)
 
-        self.combined = output.get()
-        
-        # Now here we start the reducers
-        i = 0
-        for chunk in chunked(self.combined.items(), ceil(len(self.combined) / self.n_reducers)):
-            file_path = self.output_dir + "/" + str(i) + ".txt"
-            i += 1
-            p = Process(target=self.reducer, args=(file_path, chunk))
-            p.start()
-            processes.append(p)
+            for p in processes:
+                p.join()
 
-        for p in processes:
-            p.join()
-
-        end_time = time.time()
-        print("Running time : " + str(end_time - start_time))
+            end_time = time.time()
+            print("Running time : " + str(end_time - start_time))
 
     def mapper(self, file_path, map, q):
         contents = ''
@@ -114,19 +120,26 @@ class Scheduler:
             for m in map_output:
                 q.put(m)
 
-        q.put(('EOF', 1))
+        q.put(("DONE", 1))
         # terminate the process
         # os.kill(os.getpid(), signal.SIGTERM)
 
     def sort(self, q, output):
-        while not q.empty():
+        # q.cancel_join_thread()
+        s = ("DONEE", 1)
+        done_count = 0
+        while done_count < self.n_mappers:
             s = q.get()
             key, value = s
             if key in self.combined:
                 self.combined[key].append(value)
             else:
                 self.combined[key] = [value]
-
+            ## update done count
+            if s[0] == "DONE":
+                done_count += 1
+        ## delete the "DONE" key
+        self.combined.pop('DONE', None)
         output.put(self.combined)
 
     def reducer(self, file_path, chunk):
