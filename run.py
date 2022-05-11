de2723c91fbccff9e5a42710e56f3590e47c8e6c
Richa Randeria <richamania2007@gmail.com>
2022-05-11 13:08:34 -0400

    updated run.py

--- run.py
+++ run.py
@@ -8,10 +8,24 @@ def main(argv):
     file_name = argv[1]
     n_mappers = int(argv[2])
     n_reducers =  int(argv[3])
-    lazy=False
+    print("================Lazy Task====================")
+    lazy=True
+    nimble=False
     sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
     sc.split_input_files()
-    rc=sc.launch_mappers(lazy)
+    rc=sc.launch_mappers(lazy,nimble)
+    print("================Eager Task====================")
+    lazy=False
+    nimble=False
+    sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
+    sc.split_input_files()
+    rc=sc.launch_mappers(lazy,nimble)
+    print("================Nimble Task====================")
+    lazy=False
+    nimble=True
+    sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
+    sc.split_input_files()
+    rc=sc.launch_mappers(lazy,nimble)
         
 
 if __name__ == "__main__":
