8ad4f0752970178a7ca4997f02c2e1cbcc598acd
Richa Randeria <richamania2007@gmail.com>
2022-05-11 15:59:44 -0400

    Updated run.py

--- run.py
+++ run.py
@@ -13,19 +13,20 @@ def main(argv):
     nimble=False
     sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
     sc.split_input_files()
-    rc=sc.launch_mappers(lazy,nimble)
-    print("================Eager Task====================")
-    lazy=False
-    nimble=False
-    sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
-    sc.split_input_files()
-    rc=sc.launch_mappers(lazy,nimble)
-    print("================Nimble Task====================")
-    lazy=False
-    nimble=True
-    sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
-    sc.split_input_files()
-    rc=sc.launch_mappers(lazy,nimble)
+    duration=sc.launch_mappers(lazy,sleep_val=0)
+    # print("================Eager Task====================")
+    # lazy=False
+    # nimble=False
+    # sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
+    # sc.split_input_files()
+    # finish_time=sc.launch_mappers(lazy,sleep_val=0)
+    # print("================Nimble Task====================")
+    # lazy=False
+    # sleep_val=finish_time-duration
+    # print("Delay for: "+str(sleep_val))
+    # sc = Scheduler(n_mappers=n_mappers, file_name=file_name, n_reducers=n_reducers)
+    # sc.split_input_files()
+    # sc.launch_mappers(lazy,sleep_val)
         
 
 if __name__ == "__main__":
